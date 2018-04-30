0f3f75bd90e85a5e3f40c572ebdb1617a0cdfaf1
Jacob Hoopes <jake.hoopes@gmail.com>
2018-04-30 15:18:22 -0600

    Initial commit

--- teensy_pir_motion_sensor.ino
+++ teensy_pir_motion_sensor.ino
@@ -1,32 +1,3 @@
-/* 
- * //////////////////////////////////////////////////
- * //making sense of the Parallax PIR sensor's output
- * //////////////////////////////////////////////////
- *
- * Switches a LED according to the state of the sensors output pin.
- * Determines the beginning and end of continuous motion sequences.
- *
- * @author: Kristian Gohlke / krigoo (_) gmail (_) com / http://krx.at
- * @date:   3. September 2006 
- *
- * kr1 (cleft) 2006 
- * released under a creative commons "Attribution-NonCommercial-ShareAlike 2.0" license
- * http://creativecommons.org/licenses/by-nc-sa/2.0/de/
- *
- *
- * The Parallax PIR Sensor is an easy to use digital infrared motion sensor module. 
- * (http://www.parallax.com/detail.asp?product_id=555-28027)
- *
- * The sensor's output pin goes to HIGH if motion is present.
- * However, even if motion is present it goes to LOW from time to time, 
- * which might give the impression no motion is present. 
- * This program deals with this issue by ignoring LOW-phases shorter than a given time, 
- * assuming continuous motion is present during these phases.
- *  
- */
-
-/////////////////////////////
-//VARS
 //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
 int calibrationTime = 30;        
 
